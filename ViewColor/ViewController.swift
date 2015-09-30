7553fa3660bd525c5982bd626ff8c25ab3e03d30
media_mac <media_mac@media-macs-macbook-pro.local>
2015-09-29 18:55:53 -0700

    Initial Commit

---
+++ ViewColor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewColor
+//
+//  Created by media_mac on 9/29/15.
+//  Copyright © 2015 media_mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ViewColorTests/ViewColorTests.swift
@@ -0,0 +1,36 @@
+//
+//  ViewColorTests.swift
+//  ViewColorTests
+//
+//  Created by media_mac on 9/29/15.
+//  Copyright © 2015 media_mac. All rights reserved.
+//
+
+import XCTest
+@testable import ViewColor
+
+class ViewColorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
